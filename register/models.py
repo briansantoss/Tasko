ae835a7a9e221aa6be0ef2e9ab4074fb2205f6c2
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-09-10 10:49:45 -0300

    Atualizando a estrutura do projeto e os gitignores

--- register/models.py
+++ register/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Users(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=100)
+    email = models.EmailField()
+    senha = models.CharField(max_length=100)
