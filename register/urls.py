20de1b0f7cd72e48d7a5208ffe91507ffa0a4043
Brian Santos <142011551+briansantoss@users.noreply.github.com>
2024-09-19 23:34:29 -0300

    Atualizando as urls e views no aplicativo register

--- register/urls.py
+++ register/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from register import views
 
 urlpatterns = [
-    path('', views.register, name ="register")
-
+    path('', views.register, name='register')
 ]
-
--- register/views.py
+++ register/views.py
@@ -8,16 +8,13 @@ def register(request):
     if request.method == "GET":
         return render(request, 'register/register.html')
 
-    username = request.POST.get('username')
+    nome_usuario = request.POST.get('username')
     email = request.POST.get('email')
     senha = request.POST.get('password')
 
-    user = User.objects.filter(username=username).first()
-
+    user = User.objects.filter(username=nome_usuario).first()
     if user:
         return render(request, 'register/register.html', {'usuario_existe': True})
 
-    user = User.objects.create_user(username=username, email=email, password=senha)
-    user.save()
-
+    user = User.objects.create_user(username=nome_usuario, email=email, password=senha)
     return render(request, 'login/login.html')
